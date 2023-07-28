781fb25ac7b1b95d8c906c971bd9ae955598c0c5
Berkay ORUÇ <berkay.oruc@ibb.gov.tr>
2023-07-28 09:32:00 +0300

    Updates

---
+++ precache-manifest.c47148ca834a305689c1992f78fada6a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3ca3e4b92e84031ac73",
+    "url": "/static/css/main.68082962.chunk.css"
+  },
+  {
+    "revision": "f3ca3e4b92e84031ac73",
+    "url": "/static/js/main.f3ca3e4b.chunk.js"
+  },
+  {
+    "revision": "693a507da74dfafb9dd3",
+    "url": "/static/js/1.693a507d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b0564961ab16133244f4fe3c174c87f",
+    "url": "/static/media/template-image.2b056496.png"
+  },
+  {
+    "revision": "4ebe31858114aa00de71304d32534a89",
+    "url": "/static/media/alihan.4ebe3185.png"
+  },
+  {
+    "revision": "95c4bb0c7ff890524e4692141e31cd64",
+    "url": "/static/media/berkay.95c4bb0c.png"
+  },
+  {
+    "revision": "ae6080be4e984f49a308b590bd050e41",
+    "url": "/static/media/meowScript.ae6080be.gif"
+  },
+  {
+    "revision": "4fdc3c1a92b14b8d62450215a84bd32e",
+    "url": "/static/media/eventMaster.4fdc3c1a.gif"
+  },
+  {
+    "revision": "bf18d0a1267edabf2fa2a8de96e7d6bc",
+    "url": "/static/media/accordion.bf18d0a1.gif"
+  },
+  {
+    "revision": "c92c74d131c0b4788860b1a05bb5d3dd",
+    "url": "/static/media/bo1.c92c74d1.png"
+  },
+  {
+    "revision": "212b1612194d26a2a4b89fc480d78534",
+    "url": "/static/media/bo2.212b1612.png"
+  },
+  {
+    "revision": "45882f63b4bf2b16dc2ebee1361e3941",
+    "url": "/static/media/bo3.45882f63.png"
+  },
+  {
+    "revision": "e92b5a1abd90da73931a3ae136ca710c",
+    "url": "/static/media/bo4.e92b5a1a.png"
+  },
+  {
+    "revision": "ab08610ee0e2b1fd3d96ff2f7d58754b",
+    "url": "/static/media/bo5.ab08610e.png"
+  },
+  {
+    "revision": "0136ffeea40609ba970cca3adc13a534",
+    "url": "/static/media/mapWallpaper.0136ffee.png"
+  },
+  {
+    "revision": "1ac6fa372781245be4a0d78967b522ab",
+    "url": "/index.html"
+  }
+];
