2eebf0376c95b29ed424f3c9c44f4b3d25dcec40
Berkay ORUÇ <berkay.oruc@ibb.gov.tr>
2023-08-02 09:30:26 +0300

    ts fix

--- src/app/[dynamic]/page.tsx
+++ src/app/[dynamic]/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function DynamicRoute({ params }) {
+export default function DynamicRoute({ params }: {params: {dynamic: string}}) {
 	return <div>Dynamic Route: {params.dynamic} </div>;
 }
