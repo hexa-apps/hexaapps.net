8a74d0e08a9a8e93b11575e926cbf4ef39c40b08
berkay <berkayoruc@hotmail.com>
2023-08-05 12:13:27 +0300

    google analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import Image from 'next/image';
 import Link from 'next/link';
+import Script from 'next/script';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-				{/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+		<>
+			<Script src="https://www.googletagmanager.com/gtag/js?id=G-49BZHMMWXC"></Script>
+			<Script id="google-analytics">
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+				
+					gtag('config', 'G-49BZHMMWXC');
+				`}
+			</Script>
+			<main className="flex min-h-screen flex-col items-center justify-between p-24">
+				<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+					{/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
 					HEllo
 					<code className="font-mono font-bold">src/app/page.tsx</code>
 				</p>
@@ -27,10 +38,10 @@ export default function Home() {
 						/>
 					</a>
 				</div> */}
-			</div>
+				</div>
 
-			<div className=" font-sans font-bold text-3xl relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-				{/* <Image
+				<div className=" font-sans font-bold text-3xl relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+					{/* <Image
 					className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
 					src="/next.svg"
 					alt="Next.js Logo"
@@ -38,75 +49,76 @@ export default function Home() {
 					height={37}
 					priority
 				/> */}
-				<span className="mr-[-6px]">HEXA</span>
-				<span className="ml-[-6px]">APPS</span>
-			</div>
+					<span className="mr-[-6px]">HEXA</span>
+					<span className="ml-[-6px]">APPS</span>
+				</div>
 
-			<div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-				<Link
-					href="/apps"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Apps{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Find in-depth information about our applications and extensions.
-					</p>
-				</Link>
+				<div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+					<Link
+						href="/apps"
+						className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+						rel="noopener noreferrer"
+					>
+						<h2 className={`mb-3 text-2xl font-semibold`}>
+							Apps{' '}
+							<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+								-&gt;
+							</span>
+						</h2>
+						<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+							Find in-depth information about our applications and extensions.
+						</p>
+					</Link>
 
-				<Link
-					href="/blog"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Blog{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Reach our writings.
-					</p>
-				</Link>
+					<Link
+						href="/blog"
+						className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+						rel="noopener noreferrer"
+					>
+						<h2 className={`mb-3 text-2xl font-semibold`}>
+							Blog{' '}
+							<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+								-&gt;
+							</span>
+						</h2>
+						<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+							Reach our writings.
+						</p>
+					</Link>
 
-				<Link
-					href="/alihan"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					rel="noopener noreferrer"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Alihan{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Simple Frontend Developer.
-					</p>
-				</Link>
+					<Link
+						href="/alihan"
+						className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+						rel="noopener noreferrer"
+					>
+						<h2 className={`mb-3 text-2xl font-semibold`}>
+							Alihan{' '}
+							<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+								-&gt;
+							</span>
+						</h2>
+						<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+							Simple Frontend Developer.
+						</p>
+					</Link>
 
-				<Link
-					rel="noopener noreferrer"
-					className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-					href="/berkay"
-				>
-					<h2 className={`mb-3 text-2xl font-semibold`}>
-						Berkay{' '}
-						<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-							-&gt;
-						</span>
-					</h2>
-					<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-						Simple Frontend Developer.
-					</p>
-				</Link>
-			</div>
-		</main>
+					<Link
+						rel="noopener noreferrer"
+						className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+						href="/berkay"
+					>
+						<h2 className={`mb-3 text-2xl font-semibold`}>
+							Berkay{' '}
+							<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+								-&gt;
+							</span>
+						</h2>
+						<p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+							Simple Frontend Developer.
+						</p>
+					</Link>
+				</div>
+			</main>
+		</>
 	);
 }
