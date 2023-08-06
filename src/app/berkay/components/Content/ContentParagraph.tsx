2aac4b9c50bb5637ac4b8e299008c4e1cf4abd51
berkay <berkayoruc@hotmail.com>
2023-08-06 10:10:41 +0300

    title

--- src/app/berkay/components/Content/ContentParagraph.tsx
+++ src/app/berkay/components/Content/ContentParagraph.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ContentParagraph = ({ children }: { children: any }) => {
-	return <p className="text-xl text-justify">{children}</p>;
+	return <p className="text-base text-justify">{children}</p>;
 };
 
 export default ContentParagraph;
