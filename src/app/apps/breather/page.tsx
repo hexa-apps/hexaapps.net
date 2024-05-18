e71ea4a74268eb56df25e2e3d7f8a1256718fe24
berkayoruc <berkayoruc@hotmail.com>
2024-05-18 23:18:12 +0300

    update review

--- src/app/apps/breather/page.tsx
+++ src/app/apps/breather/page.tsx
@@ -2,6 +2,7 @@ import screenshot from "../assets/screenshot-breather.png";
 import Layout from "../../components/Layout";
 import Header from "../../components/Header";
 import AppContent from "../components/AppContent";
+import { useEffect } from "react";
 
 export const metadata = {
   title: "Breather",
@@ -11,6 +12,16 @@ export const metadata = {
 export default function Apps() {
   const appURL = "https://apps.apple.com/us/app/lung-test/id6473705672";
   const appName = "Breather";
+
+  useEffect(() => {
+    // href query parameter has share link
+    const urlParams = new URLSearchParams(window.location.search || "");
+    const review = urlParams.get("review");
+    if (review) {
+      window.open(appURL, "_blank");
+    }
+  }, []);
+
   return (
     <Layout>
       <Header appName={appName} appURL={appURL} backPath="/apps" />
